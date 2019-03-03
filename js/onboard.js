4e2a7b74b7d8c891f20cfabf7238b157e209e108
nanlar <46804475+nanlar@users.noreply.github.com>
2019-03-03 15:00:08 -0600

    Edit typo and added some js function.

--- js/onboard.js
+++ js/onboard.js
@@ -3,3 +3,16 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+// my functions
+function checksubmit(){
+    alert('Your Submission Received: Congratulations, you are now signed up for our newsletter!');
+    
+    // disable submit button;
+    var submitbutton = document.getElementById("submit");
+    submitbutton.style.opacity = 0.2;
+    submitbutton.style.backgroundColor = lightgrey;
+};
+
+document.getElementById("Test1").addEventListener('submit', checksubmit);
+
